a46f8e4185e42b0adb466a953dc5e26edb0ed4a6
KevinShen <kevinyeti@gmail.com>
2018-03-18 22:44:19 +0800

    WIP:MultiTree

---
+++ src/MultiTree.Cocohub/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiTree.Cocohub
+{
+    public class Node
+    {
+        public int Index { get; set; }
+
+        public long Time { get; set; }
+
+        public Node Parent { get; set; }
+
+        public List<Node> Children { get; set; }
+
+        public Node(int index)
+        {
+            Index = index;
+        }
+
+        public string Span => (Parent == null ? "0" : Parent.Span) + "." + Index.ToString();
+    }
+}
---
+++ src/MultiTree.Cocohub/Tree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MultiTree.Cocohub
+{
+    public class Tree
+    {
+        public string Id { get; set; }
+
+        public Node Root { get; set; }
+
+        public Tree()
+        {
+            Root = new Node(1) { Parent = null };
+        }
+
+        public bool TryPeek(string span, out Node node)
+        {
+            node = null;
+            var spans = span.Split(new string[] {"."}, StringSplitOptions.RemoveEmptyEntries);
+            if (string.IsNullOrEmpty(span) || spans.Length == 0 || spans.Length == 1)
+                return false;
+            else if (spans.Length == 2)
+            {
+                if (spans[1] != "1")
+                    return false;
+                else
+                {
+                    //0.1
+                    node = Root;
+                    return true;  
+                }              
+            }
+            else
+            {
+                //0.1....
+                Node nParent = Root;
+                for (int i = 2; i < spans.Length; i++)
+                {
+                    
+                }
+            }
+
+            return false;
+        }
+    }
+}
