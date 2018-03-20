9408f9f417872a39694fd92cbc8ea5a547e7cc97
KevinShen <kevinyeti@gmail.com>
2018-03-20 21:56:58 +0800

    WIP:pgwriter

---
+++ src/Agent.Cocohub/Entity/LogEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Agent.Cocohub.Entity
+{
+    public class LogEntity
+    {
+        public string Method { get; set; }
+        public string Params { get; set; }
+        public string Action { get; set; }
+        public int Time { get; set; }
+        public DateTime LogTime { get; set; }
+    }
+}
---
+++ src/Agent.Cocohub/PgProvider/PgWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Agent.Cocohub.Entity;
+using Npgsql;
+using PostgreSQLCopyHelper;
+
+namespace Agent.Cocohub.PgProvider
+{
+    public static class PgWriter
+    {
+        static PgWriter()
+        {
+            var copyHelper = new PostgreSQLCopyHelper<LogEntity>("sample", "unit_test")
+                .MapText("Method", x => x.Method)
+                .MapText("Params", x => x.Params)
+                .MapText("Action", x => x.Action)
+                .MapInteger("Time", x => x.Time)
+                .MapDate("LogTime", x => x.LogTime);
+        }
+
+        public static void WriteToDatabase(PostgreSQLCopyHelper<LogEntity> copyHelper, IEnumerable<LogEntity> entities)
+        {
+            using (var connection = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=sampledb;User Id=philipp;Password=test_pwd;"))
+            {
+                connection.Open();
+
+                copyHelper.SaveAll(connection, entities);
+            }
+        }
+    }
+}
