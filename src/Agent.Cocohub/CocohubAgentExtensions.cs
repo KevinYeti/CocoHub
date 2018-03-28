fbd13c0d973f7ec1fb2c6585a52ed050045dbf5e
Kevin <kevinyeti@gmail.com>
2018-03-28 13:43:53 +0800

    add Agent.Cocohub

---
+++ src/Agent.Cocohub/CocohubAgentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Agent.Cocohub.Entity;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agent.Cocohub
+{
+    public static class CocohubAgentExtensions
+    {
+        public static IApplicationBuilder UseCocohubAgent(this IApplicationBuilder builder, Action<IEnumerable<LogEntity>> write)
+        {
+            Startup.StartWatch(write);
+            return builder;
+        }
+
+    }
+}
---
+++ src/Agent.Cocohub/Log/LogResolver.cs
@@ -0,0 +1,37 @@
+﻿using Agent.Cocohub.Entity;
+using System;
+
+namespace Agent.Cocohub.Log
+{
+    public static class LogResolver
+    {
+        public static bool TryResolve(string log, out LogEntity entity)
+        {
+            entity = null;
+            if (string.IsNullOrEmpty(log))
+                return false;
+
+            var parts = log.Split(new string[] { "] " }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+                return false;
+
+            try
+            {
+                entity = DynamicJson.Deserialize<LogEntity>(parts[2].Trim());
+
+                entity.LogTime = DateTime.Parse(parts[0].Trim().Replace("[", string.Empty).Replace("]", string.Empty));
+
+                entity.Level = parts[1].Trim().Replace("[", string.Empty).Replace("]", string.Empty);
+
+                entity.HasException = parts[2].ToLower().Contains("exception");
+            }
+            catch
+            {
+                entity = null;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
